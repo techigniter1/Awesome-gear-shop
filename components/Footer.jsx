3c9b31d1c07aaed52e2995360875f56a5c72367f
techigniter1 <rishabhyaduvanshi261@gmail.com>
2025-11-10 22:38:05 +0530

    chore: update footer credit to Rishabh Yadav (2025)

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,8 +4,8 @@ import { AiFillGithub } from "react-icons/ai";
 const Footer = () => {
 	return (
 		<div className="footer-container">
-			<p>Site made by Vladimir Infante</p>
-			<p>Copyright © 2023 Awesome Gear. All rights reserved</p>
+			<p>Site made by Rishabh Yadav</p>
+			<p>Copyright © 2025 Awesome Gear. All rights reserved</p>
 			<p className="icons">
 				<a
 					href="https://github.com/InfVlad"
